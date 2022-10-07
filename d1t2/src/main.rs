e06893ac2aea8ac0d772b35b745b2c16e0fc7b52
Kacper Kafara <kacperkafara@gmail.com>
2022-10-07 13:17:53 +0200

    Add solution for d1t2

--- d1t2/src/main.rs
+++ d1t2/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let input_data: Vec<i32> = include_str!("../input.txt")
+        .lines()
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect();
+
+    // S_i = a_i + a_{i + 1} + a_{i + 2}
+    // S_{i + 1} = a_{i + 1} + a_{i + 2} + a_{i + 3}
+    // S_{i + 1} - S_i = a_{i + 3} - a_i
+    // for all i in 0..(n - 3)
+
+    let mut count = 0;
+    for i in 3..input_data.len() {
+        if input_data[i] - input_data[i - 3] > 0 {
+            count += 1
+        }
+    }
+    println!("{}", count);
 }
