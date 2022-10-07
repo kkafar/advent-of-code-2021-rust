7cf488ca7e7ee8837d4f509028bdb631ab7846a1
Kacper Kafara <kacperkafara@gmail.com>
2022-10-07 17:34:22 +0200

    Cleanup d3t1 a bit
    Squashed commit of the following:
    
    commit 19e56e557d4e8e0b6b221b633398bd019ca6dcfd
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Fri Oct 7 17:32:54 2022 +0200
    
        More house keeping
    
    commit cf9d3fe01ab5c0595760a0061f75f6f7c56558ae
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Fri Oct 7 17:24:06 2022 +0200
    
        Add comments
    
    commit f53e844f1da0ca814e7d0bd4573c208caef14610
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Fri Oct 7 17:21:36 2022 +0200
    
        Take advantage of information of input shape

--- d3t1/src/main.rs
+++ d3t1/src/main.rs
@@ -1,62 +1,44 @@
+const INPUT_LINE_COUNT: i32 = 1000;
+const INPUT_LINE_LENGTH: usize = 12;
+
 fn main() {
-    let lines_iter = include_str!("../input.txt")
-        .lines();
+    // using vec![0, INPUT_LINE_LENGTH] caused vec to have size 2 despite havinc
+    // capacity INPUT_LINE_LENGTH
+    let mut buffer: Vec<i32> = Vec::with_capacity(INPUT_LINE_LENGTH);
+    for _ in 0..INPUT_LINE_LENGTH { buffer.push(0); }
 
-    let first_number = lines_iter.clone().next().unwrap();
-
-    // we assume here that lines are of fixed length.
-    let number_length = first_number.len();
-
-    // println!("Number length: {}", number_length);
-
-    let mut buffer: Vec<i32> = Vec::with_capacity(number_length);
-    for i in 0..number_length { buffer.push(0); }
-
-    // println!("Buffer length: {}", buffer.len());
-    let mut line_count = 0;
-   
-    for line in lines_iter {
-        for (i, c) in line.chars().enumerate() {
-            // println!("{}", c);
-            if c == '1' {
-                buffer[i] += 1;
+    include_str!("../input.txt")
+        .lines()
+        .for_each(|line| {
+            for (i, c) in line.chars().enumerate() {
+                if c == '1' {
+                    buffer[i] += 1;
+                }
             }
-        }
-        line_count += 1;
-    } 
+        });
 
-    let halved_line_count = line_count / 2 + line_count % 2;
+    // We notice here, that INPUT_LINE_COUNT is even => we do not need to add one.
+    let halved_line_count = INPUT_LINE_COUNT / 2;
+    let mut gamma_rate_bin: String = String::with_capacity(INPUT_LINE_LENGTH);
+    let mut eps_rate_bin: String = String::with_capacity(INPUT_LINE_LENGTH);
 
-    println!("halved_line_count: {}", halved_line_count);
-    println!("line_count: {}", line_count / 2 + line_count & 1);
-
-    let gamma_rate_bin = buffer.into_iter().map(|one_count| {
-        if one_count >= halved_line_count {
-            return '1';
-        } else {
-            return '0';
-        }
-    }).collect::<String>();
-
-    let eps_rate_bin = gamma_rate_bin.chars().map(|c| {
-        if c == '1' {
-            return '0';
-        } else {
-            return '1';
-        }
-    }).collect::<String>();
-
-    println!("gamma_rate_bin: {}", gamma_rate_bin);
-    println!("eps_rate_bin: {}", eps_rate_bin);
+    buffer
+        .into_iter()
+        .for_each(|one_count| {
+            if one_count >= halved_line_count {
+                gamma_rate_bin.push('1');
+                eps_rate_bin.push('0');
+            } else {
+                gamma_rate_bin.push('0');
+                eps_rate_bin.push('1');
+            }
+        });
 
     let gamma_rate = i32::from_str_radix(&gamma_rate_bin, 2)
         .expect("Not a binary number");
 
     let eps_rate = i32::from_str_radix(&eps_rate_bin, 2)
         .expect("Not a binary number");
-
-    println!("gamma_rate: {}", gamma_rate);
-    println!("eps_rate: {}", eps_rate);
 
     println!("{}", gamma_rate * eps_rate);
 }
