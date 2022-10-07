62c14987d8f179ee6498bfcde4948aa555d4fac9
Kacper Kafara <kacperkafara@gmail.com>
2022-10-07 13:54:58 +0200

    Solution for d2t1
    This surely is not the easiest and the most straightforward way to solve
    the problem, but I wanted to test out some trait features.

--- d2t1/src/main.rs
+++ d2t1/src/main.rs
@@ -1,3 +1,54 @@
+use std::ops::AddAssign;
+
+struct Position {
+    x: i32,
+    depth: i32,
+}
+
+struct PositionDelta<'a> {
+    direction: &'a str,
+    magnitude: i32,
+}
+
+impl<'a> AddAssign<PositionDelta<'a>> for Position {
+
+    fn add_assign(&mut self, rhs: PositionDelta) {
+        return match rhs.direction {
+            "forward" => {
+                self.x += rhs.magnitude;
+            },
+            "down" => {
+                self.depth += rhs.magnitude;
+            },
+            "up" => {
+                self.depth -= rhs.magnitude;
+            },
+            &_ => {
+                panic!("Unknown direction type");
+            }
+        }
+    }
+}
+
+impl<'a> Into<PositionDelta<'a>> for &'a str {
+    fn into(self) -> PositionDelta<'a> {
+        let splitted = self.split(' ')
+            .collect::<Vec<&str>>();
+        
+        return PositionDelta { direction: splitted[0], magnitude: splitted[1].parse::<i32>().unwrap() }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut initial_pos = Position { x: 0, depth: 0 };
+
+    include_str!("../input.txt")
+        .lines()
+        .for_each(|line| {
+            let pos_delta: PositionDelta = line.into();
+            initial_pos += pos_delta
+        });
+
+    println!("{}", initial_pos.x * initial_pos.depth);
 }
