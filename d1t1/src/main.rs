bab4818e251b44b840d6a195f0ba62ff461510c5
Kacper Kafara <kacperkafara@gmail.com>
2022-10-07 12:24:10 +0200

    Add solution d1t1

--- d1t1/src/main.rs
+++ d1t1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{path::Path, io, fs};
+
+fn main() {    
+    let input_path = Path::new("input.txt");
+    let input_data: Vec<i32> = fs::read_to_string(input_path)
+        .unwrap()
+        .split('\n')
+        .filter_map(|num| {
+            num.parse::<i32>().ok()
+        })
+        .collect();
+
+    let mut counter = 0;
+    for i in 1..input_data.len() {
+        if input_data[i] - input_data[i - 1] > 0 {
+            counter += 1;
+        }
+    }
+
+    println!("Result is: {}", counter);
 }
